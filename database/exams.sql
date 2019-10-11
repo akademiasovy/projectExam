908c2ecc94b5ede0c14a4e47564f8d729e150976
akademiasovy <roland.onofrej@akademiasovy.sk>
2019-10-11 19:40:06 +0200

    Add files via upload

--- database/exams.sql
+++ database/exams.sql
@@ -1,28 +1,35 @@
--- MySQL dump 10.16  Distrib 10.1.29-MariaDB, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: exams
--- ------------------------------------------------------
--- Server version	10.1.29-MariaDB-6+b1
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 11, 2019 at 05:38 PM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `exams`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `answer`
 --
 
 DROP TABLE IF EXISTS `answer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `answer` (
+CREATE TABLE IF NOT EXISTS `answer` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idquestion` int(11) DEFAULT NULL,
   `name` varchar(196) DEFAULT NULL,
@@ -30,26 +37,37 @@ CREATE TABLE `answer` (
   PRIMARY KEY (`id`),
   KEY `idquestion` (`idquestion`) USING BTREE
 ) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `answer`
 --
 
-LOCK TABLES `answer` WRITE;
-/*!40000 ALTER TABLE `answer` DISABLE KEYS */;
-INSERT INTO `answer` VALUES (9,1,'1',1),(10,1,'2',0),(11,1,'3',0),(12,1,'4',0),(13,2,'1',0),(14,2,'2',1),(15,2,'3',0),(16,2,'4',0),(17,3,'1',0),(18,3,'2',0),(19,3,'3',1),(20,3,'4',0),(21,4,'1',0),(22,4,'2',0),(23,4,'3',0),(24,4,'4',1);
-/*!40000 ALTER TABLE `answer` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `answer` (`id`, `idquestion`, `name`, `correct`) VALUES
+(9, 1, '1', 1),
+(10, 1, '2', 0),
+(11, 1, '3', 0),
+(12, 1, '4', 0),
+(13, 2, '1', 0),
+(14, 2, '2', 1),
+(15, 2, '3', 0),
+(16, 2, '4', 0),
+(17, 3, '1', 0),
+(18, 3, '2', 0),
+(19, 3, '3', 1),
+(20, 3, '4', 0),
+(21, 4, '1', 0),
+(22, 4, '2', 0),
+(23, 4, '3', 0),
+(24, 4, '4', 1);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `assignedquestion`
 --
 
 DROP TABLE IF EXISTS `assignedquestion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `assignedquestion` (
+CREATE TABLE IF NOT EXISTS `assignedquestion` (
   `id` int(11) NOT NULL,
   `idexam` int(11) DEFAULT NULL,
   `idstudent` int(11) DEFAULT NULL,
@@ -60,25 +78,15 @@ CREATE TABLE `assignedquestion` (
   KEY `idstudent` (`idstudent`),
   KEY `idquestion` (`idquestion`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `assignedquestion`
---
-
-LOCK TABLES `assignedquestion` WRITE;
-/*!40000 ALTER TABLE `assignedquestion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `assignedquestion` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `credentials`
 --
 
 DROP TABLE IF EXISTS `credentials`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `credentials` (
+CREATE TABLE IF NOT EXISTS `credentials` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idstudent` int(11) DEFAULT NULL,
   `login` varchar(32) DEFAULT NULL,
@@ -88,26 +96,22 @@ CREATE TABLE `credentials` (
   PRIMARY KEY (`id`),
   KEY `idstudent` (`idstudent`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `credentials`
 --
 
-LOCK TABLES `credentials` WRITE;
-/*!40000 ALTER TABLE `credentials` DISABLE KEYS */;
-INSERT INTO `credentials` VALUES (2,3,'rm','92BC521FD71C550135C7F0179026ABBFB27BD535E4676872D5A9EB86DF467DE3','D3498B4C50E5C197F6F1AE9DA52E51BC',4096);
-/*!40000 ALTER TABLE `credentials` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `credentials` (`id`, `idstudent`, `login`, `password`, `salt`, `iterations`) VALUES
+(2, 3, 'rm', '92BC521FD71C550135C7F0179026ABBFB27BD535E4676872D5A9EB86DF467DE3', 'D3498B4C50E5C197F6F1AE9DA52E51BC', 4096);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `exam`
 --
 
 DROP TABLE IF EXISTS `exam`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `exam` (
+CREATE TABLE IF NOT EXISTS `exam` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) DEFAULT NULL,
   `description` varchar(500) DEFAULT NULL,
@@ -116,50 +120,50 @@ CREATE TABLE `exam` (
   `end` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `exam`
 --
 
-LOCK TABLES `exam` WRITE;
-/*!40000 ALTER TABLE `exam` DISABLE KEYS */;
-INSERT INTO `exam` VALUES (1,'Random test','A test.',30,'2019-05-31 00:00:00','2019-11-26 00:00:00'),(2,'Random test 2','A test.',30,'2019-05-31 00:00:00','2019-11-26 00:00:00'),(3,'Test for tercia','A test.',30,'2019-05-31 00:00:00','2019-11-26 00:00:00'),(4,'Random test 3','A test.',30,'2018-05-31 00:00:00','2019-11-26 00:00:00'),(5,'Simple Exam','A simple exam with only 3 questions!',3,'2019-10-04 00:00:00','2019-12-23 00:00:00');
-/*!40000 ALTER TABLE `exam` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `exam` (`id`, `name`, `description`, `questions`, `start`, `end`) VALUES
+(1, 'Random test', 'A test.', 30, '2019-05-31 00:00:00', '2019-11-26 00:00:00'),
+(2, 'Random test 2', 'A test.', 30, '2019-05-31 00:00:00', '2019-11-26 00:00:00'),
+(3, 'Test for tercia', 'A test.', 30, '2019-05-31 00:00:00', '2019-11-26 00:00:00'),
+(4, 'Random test 3', 'A test.', 30, '2018-05-31 00:00:00', '2019-11-26 00:00:00'),
+(5, 'Simple Exam', 'A simple exam with only 3 questions!', 3, '2019-10-04 00:00:00', '2019-12-23 00:00:00');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `groups`
 --
 
 DROP TABLE IF EXISTS `groups`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `groups` (
+CREATE TABLE IF NOT EXISTS `groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `groups`
 --
 
-LOCK TABLES `groups` WRITE;
-/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-INSERT INTO `groups` VALUES (1,'prima'),(2,'sekunda'),(3,'tercia'),(4,'kvarta'),(5,'kvinta');
-/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `groups` (`id`, `name`) VALUES
+(1, 'prima'),
+(2, 'sekunda'),
+(3, 'tercia'),
+(4, 'kvarta'),
+(5, 'kvinta');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `grouptoexam`
 --
 
 DROP TABLE IF EXISTS `grouptoexam`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `grouptoexam` (
+CREATE TABLE IF NOT EXISTS `grouptoexam` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idgroup` int(11) DEFAULT NULL,
   `idexam` int(11) DEFAULT NULL,
@@ -167,104 +171,97 @@ CREATE TABLE `grouptoexam` (
   KEY `idgroup` (`idgroup`),
   KEY `idexam` (`idexam`)
 ) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `grouptoexam`
 --
 
-LOCK TABLES `grouptoexam` WRITE;
-/*!40000 ALTER TABLE `grouptoexam` DISABLE KEYS */;
-INSERT INTO `grouptoexam` VALUES (1,5,1),(2,5,2),(3,5,3),(4,5,4),(5,5,5);
-/*!40000 ALTER TABLE `grouptoexam` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `grouptoexam` (`id`, `idgroup`, `idexam`) VALUES
+(1, 5, 1),
+(2, 5, 2),
+(3, 5, 3),
+(4, 5, 4),
+(5, 5, 5);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `question`
 --
 
 DROP TABLE IF EXISTS `question`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `question` (
+CREATE TABLE IF NOT EXISTS `question` (
   `id` int(11) NOT NULL,
   `name` varchar(196) DEFAULT NULL,
   `idexam` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `idexam` (`idexam`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `question`
 --
 
-LOCK TABLES `question` WRITE;
-/*!40000 ALTER TABLE `question` DISABLE KEYS */;
-INSERT INTO `question` VALUES (1,'Select 1:',5),(2,'Select 2:',5),(3,'Select 3:',5),(4,'Select 4:',5);
-/*!40000 ALTER TABLE `question` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `question` (`id`, `name`, `idexam`) VALUES
+(1, 'Select 1:', 5),
+(2, 'Select 2:', 5),
+(3, 'Select 3:', 5),
+(4, 'Select 4:', 5);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `result`
 --
 
 DROP TABLE IF EXISTS `result`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `result` (
+CREATE TABLE IF NOT EXISTS `result` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idstudent` int(11) DEFAULT NULL,
   `idexam` int(11) DEFAULT NULL,
   `correct` int(11) DEFAULT NULL,
+  `questions` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `idexam` (`idexam`),
   KEY `idstudent` (`idstudent`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `result`
 --
 
-LOCK TABLES `result` WRITE;
-/*!40000 ALTER TABLE `result` DISABLE KEYS */;
-/*!40000 ALTER TABLE `result` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `result` (`id`, `idstudent`, `idexam`, `correct`, `questions`) VALUES
+(7, 3, 5, 3, 3);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `student`
 --
 
 DROP TABLE IF EXISTS `student`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `student` (
+CREATE TABLE IF NOT EXISTS `student` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(32) DEFAULT NULL,
   `lastname` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `student`
 --
 
-LOCK TABLES `student` WRITE;
-/*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES (3,'Richard','Miscik');
-/*!40000 ALTER TABLE `student` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `student` (`id`, `firstname`, `lastname`) VALUES
+(3, 'Richard', 'Miscik');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `studenttogroup`
 --
 
 DROP TABLE IF EXISTS `studenttogroup`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `studenttogroup` (
+CREATE TABLE IF NOT EXISTS `studenttogroup` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idstudent` int(11) DEFAULT NULL,
   `idgroup` int(11) DEFAULT NULL,
@@ -272,25 +269,15 @@ CREATE TABLE `studenttogroup` (
   KEY `idstudent` (`idstudent`),
   KEY `idgroup` (`idgroup`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `studenttogroup`
 --
 
-LOCK TABLES `studenttogroup` WRITE;
-/*!40000 ALTER TABLE `studenttogroup` DISABLE KEYS */;
-INSERT INTO `studenttogroup` VALUES (2,3,5);
-/*!40000 ALTER TABLE `studenttogroup` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `studenttogroup` (`id`, `idstudent`, `idgroup`) VALUES
+(2, 3, 5);
+COMMIT;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-10-10 19:07:07
